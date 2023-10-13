7fa9412ba88e4dbc40c03f67ef13be64cafb4fca
bitwopi <donmormajor@gmail.com>
2023-10-13 10:42:50 +0300

    added @ResponseStatus to exceptions

--- src/main/java/com/example/EmployeeProject/service/exceptions/EmployeeStorageIsFullException.java
+++ src/main/java/com/example/EmployeeProject/service/exceptions/EmployeeStorageIsFullException.java
@@ -1,5 +1,9 @@
 package com.example.EmployeeProject.service.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
 public class EmployeeStorageIsFullException extends RuntimeException{
     public EmployeeStorageIsFullException(String message){
         super(message);
